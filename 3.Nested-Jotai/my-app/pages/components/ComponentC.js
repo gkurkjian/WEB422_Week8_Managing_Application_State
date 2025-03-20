61358cd13d05e64098ca8593695ea29c3699e101
George K <gkurkjian@myseneca.ca>
2025-03-19 21:09:38 -0400

    fourth commit: Replaced React Context with Jotai for state management. Created a countAtom in store.js to manage state globally. Components now use useAtom(countAtom) to read and update state independently. Unlike Context API, only components using countAtom re-render, preventing unnecessary updates. This improves performance by ensuring only relevant components update when the state changes.

--- 3.Nested-Jotai/my-app/pages/components/ComponentC.js
+++ 3.Nested-Jotai/my-app/pages/components/ComponentC.js
@@ -5,12 +5,12 @@ export default function ComponentC(){
   console.log("Component(C) Rendered");
 
   // const [count, setCount] = useAtom(countAtom);  // this is legit but, since we're using only [ setCount ], so no need for setCount. Look line below
-  const [ setCount ] = useAtom(countAtom);
+  const [ , setCount ] = useAtom(countAtom);
 
   return (
     <>
       Component C<br />
-      <button onClick={e => setCount(count + 1) }>Increase Value</button>
+      <button onClick={e => setCount(count => count + 1) }>Increase Value</button>
     </>
   );
 }
